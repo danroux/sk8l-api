17c7a4caa7721071f311333cfb125c2edef97fa2
Daniel Roux <danroux@gmail.com>
2024-04-12 18:06:52 +0000

    Extend smoke tests (#35)

---
+++ ci/api_smoke_tests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+CA_CERT_FILE="ca-cert.pem"
+EXPECTED_ANN_COUNT=19
+EXPECTED_CRONJOBS_COUNT=3
+
+echo "Running test..."
+
+kubectl get secrets -n sk8l sk8l-tls-server-cert -o "jsonpath={.data['ca\.crt']}" | base64 --decode > $CA_CERT_FILE
+
+ann=$(grpcurl -cacert $CA_CERT_FILE -import-path protos -proto sk8l.proto -d '{}' localhost:9080 sk8l.Cronjob/GetDashboardAnnotations | jq -rc '.annotations' | jq '[.panels[] | select(.targets != null) | .targets[].expr]')
+ann_length=$(echo "$ann" | jq 'length')
+
+if [ "$ann_length" -ne "$EXPECTED_ANN_COUNT" ]; then
+  echo "Error: Expected $EXPECTED_ANN_COUNT .targets[].expr values, but got $ann_length"
+  exit 1
+else
+  echo "Success: $ann_length .targets[].expr values found, as expected."
+fi
+
+echo "----------------------------"
+
+grpcurl -cacert $CA_CERT_FILE -import-path protos -proto sk8l.proto -d '{}' localhost:9080 sk8l.Cronjob/GetCronjobs > cronjobs.json &
+pid=$!
+sleep 5
+kill $pid
+
+cronjobs_count=$(jq -r '[.cronjobs[].name] | length' cronjobs.json)
+
+if [ "$cronjobs_count" -ne "$EXPECTED_CRONJOBS_COUNT" ]; then
+  echo "Error: Expected $EXPECTED_CRONJOBS_COUNT cronjobs, but got $cronjobs_count"
+  exit 1
+else
+  echo "Success: $cronjobs_count cronjobs found, as expected."
+fi
+
+echo "----------------------------"
